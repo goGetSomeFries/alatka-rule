40a5065cacf7e07a548f5964acf9c9133f2d6e02
mocker <liuyibing123456@126.com>
2024-10-17 19:18:49 +0800

    init

--- src/test/java/com/alatka/rule/Test1.java
+++ src/test/java/com/alatka/rule/Test1.java
@@ -1,12 +1,12 @@
 package com.alatka.rule;
 
-import com.alatka.messages.util.YamlUtil;
+import com.alatka.rule.datasource.DataSourceBuilderFactory;
+import com.alatka.rule.datasource.DatabaseDataSourceBuilder;
 import com.alatka.rule.definition.RuleDefinitionBuilder;
 import com.alatka.rule.definition.YamlRuleDefinitionBuilder;
-import com.alatka.rule.util.FileUtil;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Path;
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,18 +18,16 @@ public class Test1 {
 //        new YamlRuleDefinitionBuilder().build();
         RuleDefinitionBuilder ruleDefinitionBuilder = new YamlRuleDefinitionBuilder();
         ruleDefinitionBuilder.build();
-        RulesEngine rulesEngine = new RulesEngine("riskAnalysis");
+
+        DataSource dataSource = null;
+        DataSourceBuilderFactory factory = DataSourceBuilderFactory.getInstance();
+        factory.init(new DatabaseDataSourceBuilder(dataSource));
 
         Map<String, Object> param = new HashMap<>();
         param.put("test", "1");
+        RulesEngine rulesEngine = new RulesEngine("riskAnalysis");
         List<String> result = rulesEngine.execute(param);
         System.out.println(result);
     }
 
-    @Test
-    public void test1() {
-        List<Path> list = FileUtil.getClasspathFiles("", "riskAnalysis.rule.yaml");
-        Map<String, Object> map = YamlUtil.getMap(list.get(0).toFile(), "alatka.rule.definition", Object.class);
-        System.out.println(map);
-    }
 }

0c64e3f3fb0db12c88213f9e4a4677cd1c926001
mocker <liuyibing123456@126.com>
2024-10-22 18:48:32 +0800

    init

--- src/main/java/com/alatka/rule/datasource/RedisExternalDataSource.java
+++ src/main/java/com/alatka/rule/datasource/RedisExternalDataSource.java
@@ -15,10 +15,11 @@ public class RedisExternalDataSource extends AbstractExternalDataSource {
     private static final String KEY_TYPE = "type";
     private static final String KEY_KEY = "key";
     private static final String KEY_HASH_KEY = "hashKey";
+    private static final String KEY_SET_KEY = "setKey";
 
-    private RedisTemplate<String, String> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
-    public RedisExternalDataSource(RedisTemplate<String, String> redisTemplate) {
+    public RedisExternalDataSource(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
@@ -29,12 +30,19 @@ public class RedisExternalDataSource extends AbstractExternalDataSource {
         String key = config.get(KEY_KEY).toString();
         switch (type) {
             case list:
+                return this.redisTemplate.opsForList().range(key, 0, -1);
             case string:
+                return this.redisTemplate.opsForValue().get(key);
             case hash:
+                Object hashKey = paramContext.get(config.get(KEY_HASH_KEY));
+                return this.redisTemplate.opsForHash().get(key, hashKey);
             case set:
+                // 用于黑白名单判断
+                Object setValue = paramContext.get(config.get(KEY_SET_KEY));
+                return this.redisTemplate.opsForSet().isMember(key, setValue);
             default:
+                throw new IllegalArgumentException("Unsupported type: " + type);
         }
-        return null;
     }
 
     @Override

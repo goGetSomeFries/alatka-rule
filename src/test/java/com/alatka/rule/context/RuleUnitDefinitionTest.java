e68bfc991d69a38e18610f83a7d38d5c29594406
mocker <liuyibing123456@126.com>
2024-10-22 11:37:42 +0800

    init

---
+++ src/test/java/com/alatka/rule/context/RuleUnitDefinitionTest.java
@@ -0,0 +1,13 @@
+package com.alatka.rule.context;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RuleUnitDefinitionTest {
+
+    @Test
+    @DisplayName("getter setter")
+    void test01() {
+
+    }
+}

1dec47b117dc72ae17ebb1568b4e79765c81990c
liuyibing123456 <liuyibing123456@126.com>
2024-10-23 22:53:10 +0800

    init

---
+++ src/test/java/com/alatka/rule/datasource/AbstractExternalDataSourceTest.java
@@ -0,0 +1,48 @@
+package com.alatka.rule.datasource;
+
+import com.alatka.rule.context.RuleDataSourceDefinition;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AbstractExternalDataSourceTest {
+
+    @Test
+    @DisplayName("getWithConfig()")
+    void test01() {
+        DefaultExternalDataSource externalDataSource = new DefaultExternalDataSource();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.getWithConfig(Collections.EMPTY_MAP, "card"), "card is required");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.getWithConfig(Collections.singletonMap("card", ""), "card"), "card is required");
+        Assertions.assertEquals("abc", externalDataSource.getWithConfig(Collections.singletonMap("card", "abc"), "card"));
+    }
+
+    @Test
+    @DisplayName("buildContext()")
+    void test02() {
+        DefaultExternalDataSource externalDataSource = Mockito.spy(new DefaultExternalDataSource());
+        Mockito.when(externalDataSource.doBuildContext(Mockito.any(), Mockito.any())).thenReturn("who");
+
+        RuleDataSourceDefinition definition = new RuleDataSourceDefinition();
+        definition.setScope(RuleDataSourceDefinition.Scope.rule);
+        Map<String, Object> paramContext = new HashMap<>();
+        Map<String, Object> result1 = externalDataSource.buildContext(definition, paramContext);
+        Assertions.assertNotSame(result1, paramContext);
+
+        definition.setScope(RuleDataSourceDefinition.Scope.request);
+        Map<String, Object> result2 = externalDataSource.buildContext(definition, paramContext);
+        Assertions.assertSame(result2, paramContext);
+
+        definition.setId("tradeDetail");
+        Map<String, Object> result3 = externalDataSource.buildContext(definition, paramContext);
+        Assertions.assertEquals("who", result3.get("tradeDetail"));
+
+        paramContext.put("tradeDetail", "cares");
+        Map<String, Object> result4 = externalDataSource.buildContext(definition, paramContext);
+        Assertions.assertEquals("cares", result4.get("tradeDetail"));
+    }
+}
--- src/test/java/com/alatka/rule/datasource/RedisExternalDataSourceTest.java
+++ src/test/java/com/alatka/rule/datasource/RedisExternalDataSourceTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RedisExternalDataSourceTest {
@@ -28,10 +27,16 @@ public class RedisExternalDataSourceTest {
         RedisTemplate<String, Object> redisTemplate = Mockito.mock(RedisTemplate.class);
         ListOperations<String, Object> listOperations = Mockito.mock(ListOperations.class);
         ValueOperations<String, Object> valueOperations = Mockito.mock(ValueOperations.class);
+        HashOperations<String, Object, Object> hashOperations = Mockito.mock(HashOperations.class);
+        SetOperations<String, Object> setOperations = Mockito.mock(SetOperations.class);
         Mockito.when(redisTemplate.opsForList()).thenReturn(listOperations);
         Mockito.when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        Mockito.when(redisTemplate.opsForHash()).thenReturn(hashOperations);
+        Mockito.when(redisTemplate.opsForSet()).thenReturn(setOperations);
         Mockito.when(listOperations.range(Mockito.any(), Mockito.anyLong(), Mockito.anyLong())).thenReturn(Collections.emptyList());
-        Mockito.when(valueOperations.get(Mockito.anyString())).thenReturn(valueOperations);
+        Mockito.when(valueOperations.get(Mockito.any())).thenReturn("1234");
+        Mockito.when(hashOperations.get(Mockito.any(), Mockito.any())).thenReturn("AAAAAA");
+        Mockito.when(setOperations.isMember(Mockito.any(), Mockito.any())).thenReturn(true);
         RedisExternalDataSource externalDataSource = new RedisExternalDataSource(redisTemplate);
 
         Map<String, String> config = new HashMap<>();
@@ -46,5 +51,32 @@ public class RedisExternalDataSourceTest {
         config.put("key", "");
         Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.doBuildContext(config, null), "key is required");
 
+        config.put("key", "xxxx");
+        Assertions.assertInstanceOf(List.class, externalDataSource.doBuildContext(config, null));
+
+        config.put("type", "string");
+        Assertions.assertEquals("1234", externalDataSource.doBuildContext(config, null));
+
+        config.put("type", "hash");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.doBuildContext(config, null), "hashKey is required");
+
+        config.put("hashKey", "");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.doBuildContext(config, null), "hashKey is required");
+
+        config.put("hashKey", "v_card");
+        Assertions.assertNull(externalDataSource.doBuildContext(config, Collections.emptyMap()));
+
+        Assertions.assertEquals("AAAAAA", externalDataSource.doBuildContext(config, Collections.singletonMap("v_card", "testing")));
+
+        config.put("type", "set");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.doBuildContext(config, null), "setValue is required");
+
+        config.put("setValue", "");
+        Assertions.assertThrows(IllegalArgumentException.class, () -> externalDataSource.doBuildContext(config, null), "setValue is required");
+
+        config.put("setValue", "v_card");
+        Assertions.assertNull(externalDataSource.doBuildContext(config, Collections.emptyMap()));
+
+        Assertions.assertTrue((Boolean) externalDataSource.doBuildContext(config, Collections.singletonMap("v_card", "testing")));
     }
 }

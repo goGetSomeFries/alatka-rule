a5887c4adefa35c9beaea7db436fcc53034bee15
mocker <liuyibing123456@126.com>
2025-08-11 11:15:42 +0800

    @Valid包路径bug修复

--- alatka-rule-admin/src/main/java/com/alatka/rule/admin/controller/RuleUnitController.java
+++ alatka-rule-admin/src/main/java/com/alatka/rule/admin/controller/RuleUnitController.java
@@ -7,10 +7,10 @@ import com.alatka.rule.admin.service.RuleUnitService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Tag(name = "规则单元")

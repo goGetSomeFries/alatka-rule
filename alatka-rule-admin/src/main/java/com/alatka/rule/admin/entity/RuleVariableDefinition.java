0e3787b6677fe487a8ad2481b284b2ccee832b5c
mocker <liuyibing123456@126.com>
2025-09-28 18:01:41 +0800

    增加规则变量相关功能

---
+++ alatka-rule-admin/src/main/java/com/alatka/rule/admin/entity/RuleVariableDefinition.java
@@ -0,0 +1,98 @@
+package com.alatka.rule.admin.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ALK_RULE_VARIABLE_DEFINITION")
+public class RuleVariableDefinition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "V_ID")
+    private Long id;
+
+    @Column(name = "V_KEY")
+    private String key;
+
+    @Column(name = "V_NAME")
+    private String name;
+
+    @Column(name = "V_DESC")
+    private String desc;
+
+    @Column(name = "V_TYPE")
+    private String type;
+
+    @Column(name = "V_ORDER")
+    private Integer order;
+
+    @Column(name = "V_ENABLED")
+    private Boolean enabled;
+
+    @Column(name = "G_KEY")
+    private String groupKey;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getGroupKey() {
+        return groupKey;
+    }
+
+    public void setGroupKey(String groupKey) {
+        this.groupKey = groupKey;
+    }
+}
---
+++ alatka-rule-admin/src/main/java/com/alatka/rule/admin/model/rulevariable/RuleVariableRes.java
@@ -0,0 +1,95 @@
+package com.alatka.rule.admin.model.rulevariable;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "规则变量响应")
+public class RuleVariableRes {
+
+    @Schema(description = "主键", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long id;
+
+    @Schema(description = "关键字", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String key;
+
+    @Schema(description = "名称", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @Schema(description = "描述")
+    private String desc;
+
+    @Schema(description = "类型", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String type;
+
+    @Schema(description = "顺序", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Integer order;
+
+    @Schema(description = "是否可用", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Boolean enabled;
+
+    @Schema(description = "规则组关键字", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String groupKey;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getGroupKey() {
+        return groupKey;
+    }
+
+    public void setGroupKey(String groupKey) {
+        this.groupKey = groupKey;
+    }
+}
